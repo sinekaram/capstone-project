e33991a9deba78ef8d0996d455d51e153f6ca779
Sineka R <sinekapkt@gmail.com>
2023-10-03 13:18:53 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Fragment } from "react";
+import Home from "./Components/Pages/Home";
+import { Route,Routes } from "react-router-dom";
+import FAQ from "./Components/Pages/FAQ";
+import Contact from "./Components/Pages/Contact";
+import About from "./Components/Pages/About";
+import EmiCalculator from "./Components/Pages/EmiCalculator";
+import CustomerSupport from "./Components/Pages/CustomerSupport";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Routes>
+        <Route path="natwestlogo" element={<Home/>}/>
+        <Route path="home" element={<Home />} />
+        <Route path="about" element={<About/>}/>
+        <Route path="faq" element={<FAQ/>}/>
+        <Route path="emiCalculator" element={<EmiCalculator/>}/>
+        <Route path="customerSupport" element={<CustomerSupport/>}/>
+        <Route path="contact" element={<Contact/>}/>
+        <Route path="*" element={<div className="text-center text-danger">Page Not Found!!</div>} />
+      </Routes>
+    </Fragment>
   );
 }
 
